385e146bef55bf620d06c107afac2d22bf978cb0
Pantelis_Myr <panteleimon.myriokefalitakis@univrses.com>
2020-02-28 15:57:37 +0100

    Labels

--- program/program.py
+++ program/program.py
@@ -27,6 +27,39 @@ from anki_vector.connection import ControlPriorityLevel
 from anki_vector import events
 import torchvision
 
+def get_encoded_labels():
+    COCO_INSTANCE_CATEGORY_NAMES = [
+        '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+        'bus',
+        'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A',
+        'stop sign',
+        'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep',
+        'cow',
+        'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella',
+        'N/A', 'N/A',
+        'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard',
+        'sports ball',
+        'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
+        'tennis racket',
+        'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+        'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot',
+        'hot dog', 'pizza',
+        'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A',
+        'dining table',
+        'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote',
+        'keyboard', 'cell phone',
+        'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+        'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+    ]
+    encoded_labels = {}
+    for num, l in enumerate(COCO_INSTANCE_CATEGORY_NAMES):
+        encoded_labels[num] = l
+
+    return encoded_labels
+
+
+
+
 def on_new_raw_camera_image(robot, event_type, event, done, model,
                             output_queue):
     image = event.image
@@ -46,6 +79,7 @@ def on_new_raw_camera_image(robot, event_type, event, done, model,
 
 def main():
         args = anki_vector.util.parse_command_args()
+        encoded_labels = get_encoded_labels()
         with anki_vector.Robot(args.serial,
                                behavior_control_level=ControlPriorityLevel.OVERRIDE_BEHAVIORS_PRIORITY) as robot:
             robot.camera.init_camera_feed()
@@ -69,7 +103,12 @@ def main():
                         detections = queue.get()
 
                     if detections:
-                        print(detections['labels'])
+                        detections["mapped_labels"] = []
+                        for i in detections['labels']:
+                            detections["mapped_labels"].append(
+                                encoded_labels[i])
+
+                        print(detections['mapped_labels'])
 
                     if not done.wait(timeout=5):
                         print("------ Did not receive a new camera image! ------")
